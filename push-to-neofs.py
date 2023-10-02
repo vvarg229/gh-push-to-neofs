d34c316295c2e2a76fe2bc04c274b912f5f1124a
Oleg Kulachenko <oleg@nspcc.ru>
2023-10-02 17:22:59 +0400

    Reformat
    Signed-off-by: Oleg Kulachenko <oleg@nspcc.ru>

--- push-to-neofs.py
+++ push-to-neofs.py
@@ -3,33 +3,46 @@ import subprocess
 import argparse
 import magic
 
-RUN_NUMBER = 'RunNumber'  # the key for the attribute
-FILE_PATH = 'FilePath'  # the key for the attribute, is the path for the static page and allure report zip files
-CONTENT_TYPE = 'ContentType'
+RUN_NUMBER = "RunNumber"  # the key for the attribute
+FILE_PATH = "FilePath"  # the key for the attribute, is the path for the static page and allure report zip files
+CONTENT_TYPE = "ContentType"
 PUT_TIMEOUT = 600  # in seconds
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Process allure reports')
-    parser.add_argument('--neofs_domain', required=True, type=str,
-                        help='NeoFS network domain, example: st1.t5.fs.neo.org')
-    parser.add_argument('--wallet', required=True, type=str, help='Path to the wallet')
-    parser.add_argument('--cid', required=True, type=str, help='Container ID')
-    parser.add_argument('--run-id', required=True, type=str, help='GitHub run ID')
-    parser.add_argument('--files-dir', type=str, help='Path to the directory with the files to be pushed'),
-    parser.add_argument('--expire-at', type=int,
-                        help='Expiration epoch. If epoch is not provided, or if it is 0, the report will be stored indefinitely',
-                        default=None)
+    parser = argparse.ArgumentParser(description="Process allure reports")
+    parser.add_argument(
+        "--neofs_domain",
+        required=True,
+        type=str,
+        help="NeoFS network domain, example: st1.t5.fs.neo.org",
+    )
+    parser.add_argument("--wallet", required=True, type=str, help="Path to the wallet")
+    parser.add_argument("--cid", required=True, type=str, help="Container ID")
+    parser.add_argument("--run-id", required=True, type=str, help="GitHub run ID")
+    parser.add_argument(
+        "--files-dir",
+        type=str,
+        help="Path to the directory with the files to be pushed",
+    ),
+    parser.add_argument(
+        "--expire-at",
+        type=int,
+        help="Expiration epoch. If epoch is not provided, or if it is 0, the report will be stored indefinitely",
+        default=None,
+    )
 
     return parser.parse_args()
 
 
 def get_password() -> str:
-    password = os.getenv('TEST_RESULTS_PASSWORD')
+    password = os.getenv("TEST_RESULTS_PASSWORD")
     return password
 
 
-def change_root_dir_to_container_id(root_directory: str, current_directory: str, run_id: str) -> str:
+def change_root_dir_to_container_id(
+    root_directory: str, current_directory: str, run_id: str
+) -> str:
     """
     The root of the directory is changing to Container ID.
     For example, if the directory is /home/varg/work/neofs-testcases/report:
@@ -69,49 +82,73 @@ def change_root_dir_to_container_id(root_directory: str, current_directory: str,
         return os.path.join(run_id, current_directory)
 
 
-def push_files_to_neofs(directory: str, neofs_domain: str, wallet: str, cid: str, run_id: str,
-                        expire_at: int, password: str) -> None:
+def push_files_to_neofs(
+    directory: str,
+    neofs_domain: str,
+    wallet: str,
+    cid: str,
+    run_id: str,
+    expire_at: int,
+    password: str,
+) -> None:
     base_cmd = (
-        f'NEOFS_CLI_PASSWORD={password} neofs-cli --rpc-endpoint {neofs_domain}:8080 '
-        f'--wallet {wallet}  object put --cid {cid} --timeout {PUT_TIMEOUT}s'
+        f"NEOFS_CLI_PASSWORD={password} neofs-cli --rpc-endpoint {neofs_domain}:8080 "
+        f"--wallet {wallet}  object put --cid {cid} --timeout {PUT_TIMEOUT}s"
     )
 
     if expire_at is not None and expire_at > 0:
-        base_cmd += f' --expire-at {expire_at}'
+        base_cmd += f" --expire-at {expire_at}"
 
     for subdir, dirs, files in os.walk(directory):
         current_dir_name = os.path.basename(subdir)
-        neofs_path_attr = change_root_dir_to_container_id(directory, current_dir_name, run_id)
+        neofs_path_attr = change_root_dir_to_container_id(
+            directory, current_dir_name, run_id
+        )
 
         for filename in files:
             filepath = os.path.join(subdir, filename)
             mime_type = magic.from_file(filepath, mime=True)
-            base_cmd_with_file = (f'{base_cmd} --file {filepath} --attributes {RUN_NUMBER}={run_id},'
-                                  f'{FILE_PATH}={neofs_path_attr}/{filename},{CONTENT_TYPE}={mime_type}')
-            print(f'Cmd: {base_cmd_with_file}')
+            base_cmd_with_file = (
+                f"{base_cmd} --file {filepath} --attributes {RUN_NUMBER}={run_id},"
+                f"{FILE_PATH}={neofs_path_attr}/{filename},{CONTENT_TYPE}={mime_type}"
+            )
+            print(f"Cmd: {base_cmd_with_file}")
 
             try:
-                compl_proc = subprocess.run(base_cmd_with_file, check=True, text=True,
-                                            stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=PUT_TIMEOUT,
-                                            shell=True)
+                compl_proc = subprocess.run(
+                    base_cmd_with_file,
+                    check=True,
+                    text=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    timeout=PUT_TIMEOUT,
+                    shell=True,
+                )
 
-                print(f'RC: {compl_proc.returncode}')
-                print(f'Output: {compl_proc.stdout}')
-                print(f'Error: {compl_proc.stderr}')
+                print(f"RC: {compl_proc.returncode}")
+                print(f"Output: {compl_proc.stdout}")
+                print(f"Error: {compl_proc.stderr}")
 
             except subprocess.CalledProcessError as e:
                 raise Exception(
-                    f'Command failed: {e.cmd}\n'
-                    f'Error code: {e.returncode}\n'
-                    f'Output: {e.output}\n'
-                    f'Stdout: {e.stdout}\n'
-                    f'Stderr: {e.stderr}\n'
+                    f"Command failed: {e.cmd}\n"
+                    f"Error code: {e.returncode}\n"
+                    f"Output: {e.output}\n"
+                    f"Stdout: {e.stdout}\n"
+                    f"Stderr: {e.stderr}\n"
                 )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     neofs_password = get_password()
 
-    push_files_to_neofs(args.files_dir, args.neofs_domain, args.wallet, args.cid, args.run_id, args.expire_at,
-                        neofs_password)
+    push_files_to_neofs(
+        args.files_dir,
+        args.neofs_domain,
+        args.wallet,
+        args.cid,
+        args.run_id,
+        args.expire_at,
+        neofs_password,
+    )
