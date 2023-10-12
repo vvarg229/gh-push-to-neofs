1df879e2ac174c1c731b1c310c083b4a3b01bebd
Oleg Kulachenko <oleg@nspcc.ru>
2023-10-12 23:56:39 +0400

    Fix in tests/test_downloads.py
    Signed-off-by: Oleg Kulachenko <oleg@nspcc.ru>

--- tests/test_downloads.py
+++ tests/test_downloads.py
@@ -1,12 +1,10 @@
 import os
 import requests
 import pytest
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, "data")
+from urllib.parse import urljoin
 
 
-def download_file(url):
+def download_file(url: str) -> str:
     response = requests.get(url)
     response.raise_for_status()
     return response.text
@@ -15,21 +13,24 @@ def download_file(url):
 @pytest.mark.parametrize(
     "path",
     [
-        "1.txt",
-        "2.txt",
-        "dir/3.txt",
-        "dir/subdir/4.txt",
-        "dir/subdir/subdir_2/5.txt",
+        "data/1.txt",
+        "data/2.txt",
+        "data/dir/3.txt",
+        "data/dir/subdir/4.txt",
+        "data/dir/subdir/subdir_2/5.txt",
     ],
 )
 def test_file_content(base_url, path):
     if base_url is None:
         pytest.skip("base_url is not provided. Provide it using --base_url option.")
+    print(f"base_url: {base_url}")
 
-    remote_content = download_file(f"{base_url}/{path}")
-    with open(os.path.join(DATA_DIR, path), "r") as local_file:
+    full_url = urljoin(base_url, path)
+    print(f"full_url: {full_url}")
+    remote_content = download_file(full_url)
+    with open(path, "r") as local_file:
         local_content = local_file.read()
 
     assert (
         remote_content == local_content
-    ), f"Contents of {base_url}/{path} and file {path} do not match."
+    ), f"Contents of {full_url} and {path} do not match."
